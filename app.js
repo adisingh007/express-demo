24bc48fd3ef9d47ff44e6ca1fee6c45cae004ec1
adisingh007 <adipratapsingh.aps@gmail.com>
2021-10-17 05:28:47 +0530

    app demo

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+app.listen(3000, (req, res) => {
+	console.log(`Server running on port 3000`);
+});
